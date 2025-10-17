82d2b4f8f45586ccd706793815db546826711fcc
n3mr1d <n3mr1d@gmail.com>
2025-10-17 10:07:57 +0700

    add speedinsight

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Donate from "./components/pages/donate.tsx";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Linktree from "./components/pages/linktree.tsx";
 import Contribution from "./components/pages/contribution.tsx";
 import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
+        <SpeedInsights />
        <Toaster/>
         <BrowserRouter>
 
