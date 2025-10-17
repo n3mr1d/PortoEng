a8e7e6e32c6466e479668d6396867548b689d465
n3mr1d <n3mr1d@gmail.com>
2025-10-17 10:12:42 +0700

    add speedinsight

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Donate from "./components/pages/donate.tsx";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Linktree from "./components/pages/linktree.tsx";
 import Contribution from "./components/pages/contribution.tsx";
 import { Toaster } from "sonner";
